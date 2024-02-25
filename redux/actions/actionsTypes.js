d6ae33678e2dc5528272d6b2820b78f2e5cc79a7
RAMMACHyc <sadik.ram12@gmail.com>
2024-02-25 15:14:19 +0100

    Add action types for teams, players, tournaments, matches, and single player

---
+++ redux/actions/actionsTypes.js
@@ -0,0 +1,5 @@
+export const GET_TEAMS = 'GET_TEAMS';
+export const GET_PLAYERS = 'GET_PLAYERS';
+export const GET_TOURNAMENTS = 'GET_TOURNAMENTS';
+export const GET_SINGLE_MATCHES = 'GET_MATCHES';
+export const GET_SINGLE_PLAYER = 'GET_SINGLE_MATCH';
